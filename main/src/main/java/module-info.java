a271f7f1c692e1455c9b4dd3d1622ccedb427de5
pobuchow <pobu1@hotmail.com>
2020-10-27 23:28:28 +0100

    added main module

---
+++ main/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module main {
+    requires spring.boot;
+    requires spring.boot.autoconfigure;
+    requires spring.context;
+    requires spring.webmvc;
+    requires spring.beans;
+    requires spring.security.config;
+    requires spring.security.core;
+    requires java.validation;
+    requires spring.data.commons;
+    requires java.persistence;
+    requires com.fasterxml.jackson.annotation;
+    requires spring.web;
+}
