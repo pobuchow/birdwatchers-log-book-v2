9762e2e52cda544669284c7220cf15b93432ea46
pobuchow <pobu1@hotmail.com>
2020-11-01 21:58:09 +0100

    configure mvn run

--- main/app/src/main/java/com/blb/app/BirdwatchersLogBookV2Application.java
+++ main/app/src/main/java/com/blb/app/BirdwatchersLogBookV2Application.java
@@ -2,8 +2,10 @@ package com.blb.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"service"})
 public class BirdwatchersLogBookV2Application {
 
     public static void main(String[] args) {
