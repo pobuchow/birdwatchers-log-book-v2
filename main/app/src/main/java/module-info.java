469529c4a92ea0689849269b6f7886c602b353a8
pobuchow <pobu1@hotmail.com>
2020-11-02 22:03:25 +0100

    multimodule bean configuration

--- main/app/src/main/java/module-info.java
+++ main/app/src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module com.blb.app {
+open module com.blb.app {
     requires spring.context;
     requires spring.beans;
     requires spring.security.core;
@@ -7,6 +7,9 @@ module com.blb.app {
     requires spring.boot;
     requires spring.boot.autoconfigure;
     requires spring.webmvc;
+    requires java.sql;
+    requires spring.data.jpa;
+    requires com.blb.controller;
 
     exports com.blb.app;
 }
